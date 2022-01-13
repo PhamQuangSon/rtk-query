1e944c75b5eafb8c48a8a9ff2611ac85bc4f77e9
Quang Sơn Phạm <phamquangson4493@gmail.com>
2022-01-13 17:43:06 +0700

    add feature login - add scss

---
+++ src/features/login/Login.tsx
@@ -0,0 +1,16 @@
+import { loginUrl } from "../../spotify";
+import './Login.scss';
+
+export function Login() {
+    return (
+        <div className="login">
+         
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+
+
+
+        </div>
+    )
+}
